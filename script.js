305d01c85f9e4488b56b71679a9d2f5642ec2eb9
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-22 18:04:24 -0600

    display the next 5 days weather

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Global variables
 let btnSearchEl = document.querySelector("#btnSearch");
 let searchInputEl = document.querySelector("#searchInput");
-let cityNameEl = document.getElementById("city-name");
-let cityDateEl = document.getElementById("city-date");
-let cityIconEl = document.getElementById("city-icon");
-let cityTempEl = document.getElementById("city-temp");
-let cityHumidityEl = document.getElementById("city-humidity");
-let cityWindSpeedEl = document.getElementById("city-windspeed");
+// let cityNameE = document.getElementById("city-name");
+// let cityDateEl = document.getElementById("city-date");
+// let cityIconEl = document.getElementById("city-icon");
+// let cityTempEl = document.getElementById("city-temp");
+// let cityHumidityEl = document.getElementById("city-humidity");
+// let cityWindSpeedEl = document.getElementById("city-windspeed");
 let divCityEl = document.querySelector("#div-city");
 
 let citiesArray = JSON.parse(localStorage.getItem("Cities")) || [];
@@ -29,15 +29,21 @@ function search() {
       console.log(data.list);
       console.log(data.list[0].main);
       console.log(data.list[0].main.temp);
-      // for (let i = 0; i < data.list.length; i++) {
-      cityNameEl.textContent = `City name: ${data.city.name}`;
-      cityDateEl.textContent = `Date: ${data.list[0].dt_txt}`;
-      // let icon = data.list[0].weather[0].icon;
-      // cityIconEl.textContent = `${icon}.png`
-      cityTempEl.textContent = `Temp: ${data.list[0].main.temp}`;
-      cityHumidityEl.textContent = `Humidity: ${data.list[0].main.humidity}`;
-      cityWindSpeedEl.textContent = `Wind Speed: ${data.list[0].wind.speed}`;
-      // }
+      for (let i = 0; i < data.list.length; i++) {
+        dtText = data.list[i].dt_txt.split(" ")[1];
+        if (dtText === "00:00:00") {
+          let cityCard = document.createElement("div");
+
+          // cityCard.innerHTML += `<h1>City name: ${data.city.name}</h1>`;
+          cityCard.innerHTML += `<h1>Date: ${data.list[i].dt_txt}</h1>`;
+          // let icon = data.list[0].weather[0].icon;
+          // cityIconEl.innerHTML = `${icon}.png`
+          cityCard.innerHTML += `<h2>Temp: ${data.list[i].main.temp}</h2>`;
+          cityCard.innerHTML += `<h2>Humidity: ${data.list[i].main.humidity}</h2>`;
+          cityCard.innerHTML += `<h2>Wind Speed: ${data.list[i].wind.speed}</h2>`;
+          divCityEl.append(cityCard);
+        }
+      }
     });
   let citiesObject = {
     city: city,
